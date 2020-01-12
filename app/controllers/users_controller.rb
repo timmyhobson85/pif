25e5fe972417c894d13b7093a7bd3f9fd0215a7a
Tim Hobson <timhobson85@gmail.com>
2020-01-12 20:34:17 +1100

    finished user login/create/delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,48 @@
 class UsersController < ApplicationController
+
+  # before_action :check_if_logged_in, except: [:new, :create]
+  # before_action :check_if_logged_in, only: [ :show, :edit, :update, :destroy ]
+  before_action :check_if_admin, only: [:index]
+
+
   def new
+    @user = User.new
   end
 
   def create
+    # raise 'hell'
+    @user = User.create user_params
+    if @user.persisted?
+      session[:user_id] = @user.id #autologin
+      redirect_to user_path( @user.id )
+    else
+      render :new
+    end
   end
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find params[:id]
   end
 
   def edit
+    @user = User.find params[:id]
   end
 
   def update
+    @user = User.find params[:id]
   end
 
   def destroy
+    User.destroy params[:id]
+    redirect_to login_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
+  root to: 'session#new'
   # sessions (login form/form submit/logout) routes:
-  get '/login' => 'session#new'
-  post '/login' => 'session#create'
+  get '/login'    => 'session#new'
+  post '/login'   => 'session#create'
   delete '/login' => 'session#destroy'
 
   resources :users
